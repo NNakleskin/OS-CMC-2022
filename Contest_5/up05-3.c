9e9b8185336e5d2e15fb303456d6b20810fc9364
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-10-21 21:40:36 +0300

    Solved

--- Contest_5/up05-3.c
+++ Contest_5/up05-3.c
@@ -6,32 +6,24 @@ const char BASE[] = "rwxrwxrwx";
 
 
 int
-parce_rwx_permissions(const char *str){ 
+parse_rwx_permissions(const char *str){
     int n = sizeof(BASE) - 1;
-    int tmp[n + 1];
     int res = 0;
     if(str == NULL){
-        return 1;
+        return -1;
     }
-    if(strlen(str) != n){
-        return 1;
+    if(strlen(str) != n){ 
+        return -1;
     }
     for(int i = 0; i < n; i++){
-        tmp[i] = (int) str[i] ^ BASE[i];
-        if(tmp[i] != 0){
-            tmp[i] = 0;
+        if(str[i] == '-'){
+            res <<= 1;
+        } else if(str[i] == BASE[i]) {
+            res <<= 1;
+            res |= 1;
         } else {
-            tmp[i] = 1;
+            return -1;
         }
-        printf("%d", tmp[i]);
     }
     return res;
-}
-
-
-int main(void){
-    char input[9];
-    scanf("%s", input);
-    printf("%o\n", parce_rwx_permissions(input));
-    return 0;
 }
