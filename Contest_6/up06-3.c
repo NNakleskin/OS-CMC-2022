191d4d0fcd6dc2e72a34d5dc5f57f38c5a339f27
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-10-24 03:10:07 +0300

    Solved up06-3

--- Contest_6/up06-3.c
+++ Contest_6/up06-3.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-enum { SEC_IN_D = 86400 };
+#include <sys/time.h>
 
 
 int main(void){
     int n;
     scanf("%d", &n);
-    time_t *diff;
-    diff = 0;
-    struct tm start;
-    start.tm_year = n - 1900;
-    time_t time_start = mktime(&start);
-    struct tm fin;
-    printf("%d\n", n);
-    fin.tm_year = n - 1899;
-    printf("%d\n", fin.tm_year);
-    time_t time_fin = mktime(&fin);
-   
-    printf("%ld\n", time_start);
-    printf("%ld\n", time_fin);
-
-    for(long i = time_start; i < time_fin; i += SEC_IN_D){
-        struct tm day;
-        gmtime_r(&i, &day);
-        printf("nnn%d\n", day.tm_wday);
+    if(n > 2037 || n < 1910) {
+        return 0;
     }
-
+// 4
+    int count_week = 0;
+    for(int month = 0; month < 12; month++) {
+        count_week = 0;
+        for(int day = 1; day < 32; day++) {
+            
+            struct tm tmp;
+            tmp.tm_year = n - 1900;
+            tmp.tm_mon = month;
+            tmp.tm_mday = day;
+            tmp.tm_hour = 0;
+            tmp.tm_min = 0;
+            tmp.tm_sec = 0;
+            tmp.tm_isdst = -1;
+            
+            mktime(&tmp);
+            if(tmp.tm_wday == 4) {
+                count_week++;
+                if(count_week == 2 || count_week == 4) {
+                    if((tmp.tm_mday) % 3 != 0) {
+                        printf("%d %d\n", tmp.tm_mon  + 1, tmp.tm_mday);
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
