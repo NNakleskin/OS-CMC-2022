594f7763ed62b6c42f60f6d5e6d5216ca369a213
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-10-24 18:01:13 +0300

    Not working, check up07-2-2.c

--- Contest_7/up07-2.c
+++ Contest_7/up07-2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 
 
 int main(int argc, char** argv)
@@ -9,8 +10,9 @@ int main(int argc, char** argv)
     if(argc < 2) {
         printf("%lld\n", res);
         return 0;
-    }   
+    }
     for(int i = 1; i < argc; i++) {
+        errno = 0;
         int len = strlen(argv[i]);
         int sign = 1;
         int k_indicate = 1;
@@ -19,13 +21,31 @@ int main(int argc, char** argv)
         }
         if(argv[i][len - 1] == '-') {
             sign *= -1;
-        }
+        } 
         argv[i][len - 1] = '\0';
         if(argv[i][len - 2] == 'k') {
             k_indicate = 1000;
             argv[i][len - 2] = '\0';
         }
-        res += atoi(argv[i]) * k_indicate * sign;
+        int indicate = 0;
+        for(int j = 1; j < strlen(argv[i]); j++) {
+            if(argv[i][j] < '0' || argv[i][j] > '9') {
+                indicate = 1;
+                break;
+            }
+        }
+        if(indicate) {
+            continue;
+        }
+        int tmp =  strtol(argv[i], NULL, 10) * sign;
+        if(errno != 0) {
+            continue;
+        }
+        if(__builtin_mul_overflow(tmp, k_indicate, &tmp)) {
+            continue;
+        } else {
+            res += tmp;
+        }
     }
     printf("%lld\n", res);
     return 0;
