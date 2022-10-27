29853611fa4520dde9f583ef033c8334a25acb6a
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-10-28 00:21:06 +0300

    Solved up06-1

--- Contest_6/up06-1.c
+++ Contest_6/up06-1.c
@@ -5,31 +5,56 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+
+const char exe[] = ".exe";
+
+
+int
+is_exe(char *path, size_t len) {
+    int index = len - sizeof(exe);
+    if(index < 0) {
+        return 0;
+    }
+    if(strcmp(&path[index], exe)) {
+        return 0;
+    }
+    return 1;
+}
+
 
 
 int main(int argc, char** argv)
 {
-    DIR *dir = opendir(argv[1]);
-    if(dir == NULL)
-    {
-        perror("opendir");
+    if (argc < 2) {
         return 0;
     }
-    struct dirent *de;
+    DIR *dir = opendir(argv[1]);
+    size_t path_len = strlen(argv[1]);
+    if(dir == NULL) {
+        return 1;
+    }
+    struct dirent *de = NULL;
     de = readdir(dir);
+    int count = 0;
     while(de){
-
-        char path[100];
-        strcpy(path, argv[1]);
-        strncat(path, "/", 1);
-        strncat(path, de->d_name, strlen(de->d_name));
-        printf("%s\n", path); 
+        size_t tmp_len = path_len + strlen(de->d_name) + 2;
+        char *path;
+        asprintf(&path, "%s/%s", argv[1], de->d_name);        
         struct stat buf;
-        lstat(path, &buf);
-        printf("size %lld\n", buf.st_size);
-        printf("reg %d\n", S_ISREG(buf.st_mode));
+        if (stat(path, &buf) < 0) {
+            free(path);
+            de = readdir(dir);
+            continue;
+        }
+        if(is_exe(path, tmp_len) && S_ISREG(buf.st_mode) && access(path, X_OK) == 0) {
+            count++;
+        }
+        free(path);
         de = readdir(dir);
     }
-    
+    closedir(dir);
+    printf("%d\n", count);
     return 0;
 }
