0b72d6593a0ec034e62652ebae63027c429a2cf3
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-10-29 00:16:30 +0300

    Save commit

--- Contest_6/up06-2.c
+++ Contest_6/up06-2.c
@@ -4,51 +4,59 @@
 #include <limits.h>
 
 
-int
-is_dot(char *buf) {
-    if(*buf == '.' && *(buf - 1) == '/') {
-        return 1;
-    }
-    return 0;
-}
-
-
-int
-is_doubledot(char *buf){
-    if(*buf == '.' && *(buf - 1) == '.' && *(buf - 2) == '/') {
-        return 1;
-    }
-    return 0;
-}
-
-
-char*
-find_slash(char *buf) {
-    while(*buf != '/') {
-        *buf = *(buf - 1);
-    }
-    return buf;
-}
-
-
 void
 normalize_path(char *buf) {
-    int doubledot_count = 0;
-    int len = strlen(buf);
+    size_t len = strlen(buf);
+    char *tmp = calloc(len, sizeof(char));
     if(buf[len - 1] == '/') {
         buf[len - 1] = '\0';
         len--;
     }
-    for(int i = len - 1; i > 1; ++i) {
-        if(i > 2) {
-            if(is_doubledot(buf[i]) {
-                doubledot_count++;
-                i -= 3;
-            } else if (is_dot(buf[i])) {
-                i -= 2;
+    int tmp_index = 0;
+    int ddot_count = 0;
+    for(int i = 0; i < len; ++i) {
+        //printf("%s\n", tmp);
+        if(i + 2 == len) {
+            if(buf[i] == '/' && buf[i + 1] == '.') {
+                i++;
+                continue;   
+            }
+        } else if(i + 3 < len) {
+            if(buf[i] == '/' && buf[i + 1] == '.' && buf[i + 2] == '/') {
+                i += 1;
+                continue;
             } 
-       }
+        }
+        if(i + 3 == len) {
+            if(buf[i] == '/' && buf[i + 1] == '.' && buf[i + 2] == '.') {
+                while(tmp[tmp_index] != '/' || tmp_index > 1) {
+                    tmp[tmp_index] = '\0';
+                    tmp_index--;
+                }
+                //tmp[tmp_index] = '\0';
+                ddot_count++;
+                i += 1;
+                continue;
+            } 
+        } else if(i + 4 < len) {
+            if(buf[i] == '/' && buf[i + 1] == '.' && buf[i + 2] == '.' && buf[i + 3] == '/') {
+                while(tmp[tmp_index] != '/' || tmp_index > 1) {
+                    tmp[tmp_index] = '\0';
+                    tmp_index--;
+                }
+                //tmp[tmp_index] = '\0';
+                ddot_count++;
+                i += 2;
+                continue;
+            }
+        }
+        tmp[tmp_index] = buf[i];
+        tmp_index++;
+        
     }
+    snprintf(buf, strlen(tmp) + 1, "%s", tmp);
+    //printf("%s\n", tmp);
+    //printf("%d\n", ddot_count);
 }
 
 
@@ -56,6 +64,6 @@ int main(void) {
     char a[PATH_MAX];
     scanf("%s", a);
     normalize_path(a);
-    printf("%s", a);
+    printf("%s\n", a);
     return 0;
 }
