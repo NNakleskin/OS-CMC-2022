b57ab4a3e5e983edf6fceeff6675843b316c8638
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-10 01:33:35 +0300

    Solved ht10-3, not tested

--- Contest_10/ht10-3.c
+++ Contest_10/ht10-3.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 // hexdump -C ht10-3.c
 
 
@@ -14,52 +13,43 @@ int main(int argc, char **argv) {
     }
     int rows = (int) strtol(argv[2], NULL, 10);
     int cols = (int) strtol(argv[3], NULL, 10);
-    int fd = open(argv[1], O_RDONLY | O_CREAT | O_TRUNC , 0777);
+    int fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC , 0777);
     if (fd == -1) {
         return 1;
     }
-    int **matrix = malloc(rows * sizeof(int*));
-    for(int i = 0; i < rows; ++i) {
-        matrix[i] = malloc(cols * sizeof(int));
-    }
+    ftruncate(fd, rows * cols * (ssize_t) sizeof(int));
+    fd = open(argv[1], O_RDWR, 0777);
+    int *matrix = mmap(NULL, rows * cols * sizeof(int), 
+            PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
     int l_up = 0, l_down = 0, r_up = cols - 1, r_down = rows - 1;
     int num = 1; 
     while(num <= rows * cols) {
         for(int i = l_down; i <= r_up; ++i) {
             if(num > rows * cols) { break; }
-            matrix[l_up][i] = num;
+            matrix[l_up * rows + i] = num;
             num++;
         }
         l_up++;
         for(int i = l_up; i <= r_down; ++i) {
             if(num > rows * cols) { break; }
-            matrix[i][r_up] = num;
+            matrix[i * rows + r_up] = num;
             num++;
         }
         r_up--;
         for(int i = r_up; i >= l_down; --i) {
             if(num > rows * cols) { break; }
-            matrix[r_down][i] = num;
+            matrix[r_down * rows + i] = num;
             num++;
         }
         r_down--;
         for(int i = r_down; i >= l_up; --i) {
             if(num > rows * cols) { break; }
-            matrix[i][l_down] = num;
+            matrix[i * rows + l_down] = num;
             num++;
         }
         l_down++;
-    } 
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        putchar(10);
     }
-    for(int i = 0; i < rows; ++i) {
-        free(matrix[i]);
-    }
-    free(matrix);
+    munmap(matrix, rows * cols);
     close(fd);
     return 0;
 }
