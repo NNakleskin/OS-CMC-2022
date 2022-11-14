c14f7a2a9b35b35f7fadc09254601a0d0906936c
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-15 00:22:20 +0300

    Created files via bash script

---
+++ Contest_10/ht10-5/ht10-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    int first_a, first_b, second_a, second_b;
+    scanf("%d%d%d%d", &first_a, &first_b, &second_a, &second_b);
+    if(first_a > )
+
+    return 0;
+}
