8a4fef0fe97bfefa6117a8820dd0fe7bbded9893
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-22 19:31:41 +0300

    Solved up12-4

--- Contest_12/up12-3.c
+++ Contest_12/up12-3.c
@@ -2,44 +2,48 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 
 int main(int argc, char **argv) {
     int status = 0;
     pid_t p;
+    int count = 0;
     for(int i = 1; i < argc; i++) {
         switch (argv[i][0]) {
             case 's':
                 while(wait(&status) > 0){
-                    if(status != 0) { _exit(1); }
+                    count += (!WEXITSTATUS(status) && WIFEXITED(status));
                 }
                 p = fork();
                 if(p == -1) {
-                    _exit(1);
+                    _exit(EXIT_FAILURE);
                 } else if(!p) {
                     execlp(&argv[i][1], &argv[i][1], NULL);
-                    _exit(1);
+                    _exit(EXIT_FAILURE);
                 }
                 wait(&status);
-                if(status != 0) {
-                    _exit(1);
-                }
+                count += (!WEXITSTATUS(status) && WIFEXITED(status));     
                 break;
             case 'p':
                 p = fork();
                 if(p == -1) {
-                    _exit(1);
+                    _exit(EXIT_FAILURE);
                 }
                 if(!p) {
                     execlp(&argv[i][1], &argv[i][1], NULL);
-                    _exit(1);
+                    _exit(EXIT_FAILURE);
                 }
                 break;
             default:
                 while(wait(NULL) > 0) {}
-                return 1;
+                _exit(EXIT_FAILURE);
         }
     }
-    while(wait(NULL) > 0) {}
-    _exit(0);
+    while(wait(&status) > 0) {
+        count += (!WEXITSTATUS(status) && WIFEXITED(status));        
+    }
+    printf("%d\n", count);
+    fflush(stdout);
+    _exit(EXIT_SUCCESS);
 }
