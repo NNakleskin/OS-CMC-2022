8a6d093f41cd9631ba568aa600bd12ec6fbf108e
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-22 19:40:47 +0300

    Solved

---
+++ Contest_10/ht10-5/gen_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for (( i=1; i < 10; i++ ))
+do
+    touch voice_$i.dat
+    touch voice_$i.ans
+done
+
+
--- Contest_10/ht10-5/ht10-5.c
+++ Contest_10/ht10-5/ht10-5.c
@@ -5,7 +5,28 @@
 int main(void) {
     int first_a, first_b, second_a, second_b;
     scanf("%d%d%d%d", &first_a, &first_b, &second_a, &second_b);
-    if(first_a > )
+
+    int res_1 = 0, res_2;
+    if(first_a > second_a) {
+        res_1 = first_a;
+    } else {
+        res_1 = second_a;
+    }
+    if(first_b > second_b) {
+        res_2 = second_b;
+    } else {
+        res_2 = first_b;
+    }
+    
+    
+
+
+    if(res_1 >= res_2) {
+        res_1 = 0;
+        res_2 = 0;
+    }
+    
+    printf("%d %d\n",res_1, res_2);
 
     return 0;
 }
