06198458993bcab1a6cbca921aeb0d25886aa62d
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-28 12:27:02 +0300

    Created template

---
+++ Contest_14/template.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+
+
+
+int main(void) {
+    
+    _exit(0);
+}
