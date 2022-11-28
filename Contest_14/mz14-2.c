e44a51e2d01ab85445605d7c965be366ec43f6f9
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-28 12:26:03 +0300

    Solved mz14-2

---
+++ Contest_14/mz14-2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+
+
+int indicate = 0;
+unsigned int res = 0;
+
+
+void handler_1(int sig) {
+    indicate = 0;
+}
+
+
+void handler_2(int sig) {
+    indicate = 1;
+}
+
+
+
+int main(void) {
+    sigaction(SIGINT, &(struct sigaction) { .sa_handler = handler_1, .sa_flags = SA_RESTART }, NULL);
+    sigaction(SIGQUIT, &(struct sigaction) { .sa_handler = handler_2, .sa_flags = SA_RESTART }, NULL);
+    pid_t pid = getpid();
+    printf("%d\n", pid);
+    fflush(stdout);
+    int n;
+    indicate = 0;
+    res = 0;
+    while(scanf("%d", &n) != EOF) {
+        if(!indicate) {
+            res += (unsigned int) n;
+            printf("%d\n", (int) res);
+            fflush(stdout);
+        } else if(indicate) {
+            res *= (unsigned int) n;
+            printf("%d\n", (int) res);
+            fflush(stdout);
+        }
+    }
+    _exit(0);
+}
