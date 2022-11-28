9af71c54b901f6001bf8163838ad85204a9f5459
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-28 17:21:43 +0300

    Solved up14-3, not tested

--- Contest_14/mz14-3.c
+++ Contest_14/mz14-3.c
@@ -6,10 +6,44 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <limits.h>
 
+
+
+int count = 0;
 
 
 int main(int argc, char **argv) {
-    
+    int n;
+    n = strtol(argv[1], NULL, 10);
+    for(int i = 2; i < n + 2 && i < argc; ++i) {
+        FILE *input = fopen(argv[i], "r");
+        char buf[PATH_MAX + 1];
+        fscanf(input, "%s", buf);
+        pid_t p = fork();
+        if(!p) {
+            execlp(buf, buf, NULL);
+            _exit(1);
+        }
+    }
+    int status;
+    while(wait(&status) > 0) {
+        count += !WEXITSTATUS(status) && WIFEXITED(status);
+    }
+    for(int i = n + 2; i < argc; i++) {
+        pid_t p = fork();
+        if(!p) {
+            FILE *input = fopen(argv[i], "r");
+            char buf[PATH_MAX + 1];
+            fscanf(input, "%s", buf);
+            execlp(buf, buf, NULL);
+            _exit(1);
+        }
+        int status;
+        wait(&status);
+        count += !WEXITSTATUS(status) && WIFEXITED(status);
+
+
+    }
     _exit(0);
 }
