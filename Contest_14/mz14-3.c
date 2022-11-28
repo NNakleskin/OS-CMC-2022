3a7ed473ccdc202149c9b7fce3521588020c9cc9
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-28 12:26:27 +0300

    Start solving mz13-4

---
+++ Contest_14/mz14-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+
+
+
+int main(int argc, char **argv) {
+    
+    _exit(0);
+}
