4f808b6e9f1a6afe11afc5dcf0b60ab955dd1f2e
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-29 01:29:51 +0300

    Fixing up14-5

--- Contest_14/up14-5.c
+++ Contest_14/up14-5.c
@@ -6,8 +6,8 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-unsigned count = 0;
-int num = 0;
+volatile long long count = 0;
+volatile long long num = 0;
 
 
 void 
@@ -16,7 +16,7 @@ handler(int sig)
     switch (sig) {
         case SIGINT:
             if (count < 3) {
-                printf("%d\n", num);
+                printf("%lld\n", num);
                 fflush(stdout);
                 ++count;
                 return;
@@ -26,6 +26,7 @@ handler(int sig)
             break;
         case SIGTERM:
             exit(EXIT_SUCCESS);
+            break;
         default: {}
     }
 }
@@ -33,22 +34,20 @@ handler(int sig)
 
 int 
 main(void) {
-    int low, high;
-    scanf("%d%d", &low, &high);
     sigaction(SIGINT, &(struct sigaction) {.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
-    sigaction (SIGTERM, &(struct sigaction) {.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
+    sigaction(SIGTERM, &(struct sigaction) {.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
+    long long low, high;
+    scanf("%lld%lld", &low, &high);
     printf("%d\n", getpid());
     fflush(stdout);
     for (int i = low; i < high; ++i) {
-        int indicate = 1;
+        if(i < 2) { continue; }
         for(int j = 2; j <= i / 2; ++j) {
             if(!(i % j)) {
-                indicate = 0;
+                continue;
             }
         }
-        if(indicate) {
-            num = i;
-        }
+        num = i;
     }
     printf("-1\n");
     fflush(stdout);
