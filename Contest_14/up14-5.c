98cd98f9c8b1e1496510c2049e9610d4e6d0afef
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-11-29 03:52:28 +0300

    Solved up14-5

--- Contest_14/up14-5.c
+++ Contest_14/up14-5.c
@@ -6,50 +6,54 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-volatile long long count = 0;
-volatile long long num = 0;
+
+volatile int count = 0;
+volatile int num = 0;
 
 
-void 
-handler(int sig)
-{
-    switch (sig) {
-        case SIGINT:
-            if (count < 3) {
-                printf("%lld\n", num);
-                fflush(stdout);
-                ++count;
-                return;
-            } else {
-                exit(EXIT_SUCCESS);
-            }
-            break;
-        case SIGTERM:
-            exit(EXIT_SUCCESS);
-            break;
-        default: {}
+int max(int f_num, int s_num) {
+    return f_num > s_num ? f_num : s_num;
+}
+
+
+void handler_1() {
+    signal(SIGINT, handler_1);
+    if (count < 3) {
+        printf("%d\n", num);
+        fflush(stdout);
+        ++count;
+    } else {
+        _exit(EXIT_SUCCESS);
     }
 }
 
 
+void handler_2() {
+    _exit(EXIT_SUCCESS);
+}
+
+
 int 
-main(void) {
-    sigaction(SIGINT, &(struct sigaction) {.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
-    sigaction(SIGTERM, &(struct sigaction) {.sa_handler = handler, .sa_flags = SA_RESTART}, NULL);
-    long long low, high;
-    scanf("%lld%lld", &low, &high);
+main(int argc, char **argv) { 
+    sigaction(SIGINT, &(struct sigaction) {.sa_handler = handler_1, .sa_flags = SA_RESTART}, NULL);
+    sigaction(SIGTERM, &(struct sigaction) {.sa_handler = handler_2, .sa_flags = SA_RESTART}, NULL);
     printf("%d\n", getpid());
     fflush(stdout);
+    int low, high;
+    scanf("%d%d", &low, &high);
     for (int i = low; i < high; ++i) {
+        int indicate = 1;
         if(i < 2) { continue; }
-        for(int j = 2; j <= i / 2; ++j) {
+        for(int j = 2; j * j <= i; ++j) {
             if(!(i % j)) {
-                continue;
+                indicate = 0;
             }
         }
-        num = i;
+        if(indicate) {
+            num = i;
+        }
     }
     printf("-1\n");
     fflush(stdout);
-    exit(EXIT_SUCCESS);
+    _exit(EXIT_SUCCESS);
 }
